654764045c2f2f85492617b637e3462eda8f9a45
Inger Klekacz <ingernet@gmail.com>
2018-06-15 11:09:36 -0700

    more ruby style tweaks

--- spec/acceptance/classes/elk_spec.rb
+++ spec/acceptance/classes/elk_spec.rb
@@ -1,12 +1,10 @@
 require 'spec_helper_acceptance'
 describe 'elk class' do
   context 'default parameters' do
-    let(:manifest){
-      "class {'elk':}"
-    }
+    let(:manifest){ "class {'elk':}" }
 
-    it 'should run without errors' do
-      result = apply_manifest(manifest, :catch_failures => true)
+    it 'runs without errors' do
+      result = apply_manifest(manifest, {:catch_failures => true})
       expect(result.exit_code).to eq 2
     end
   end
